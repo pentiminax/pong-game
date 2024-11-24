6302be5dcded949e9c48a3a5006a8a2e0f6fde05
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2024-11-24 19:24:47 +0100

    Fix server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,14 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+
+const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:5173', // Autorisez votre client
+      methods: ['GET', 'POST'],
+      credentials: true,
+    },
+  });
 
 const PORT = 3000;
 
